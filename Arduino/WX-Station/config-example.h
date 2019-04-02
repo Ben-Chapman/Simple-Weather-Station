4725fdc488fafc4165c5df8dab7a394db7cf4bc9
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2019-04-02 10:50:29 -0400

    Better management of config files

--- Arduino/WX-Station/config-example.h
+++ Arduino/WX-Station/config-example.h
@@ -1,10 +1,9 @@
 // Adafruit.io Config
-// Adafruit IO
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883
 #define AIO_USERNAME    ""
 #define AIO_KEY         ""
 
-// Wifi Config
-char ssid[] = "";   //  your network SSID (name)
-char pass[] = "";  // your network password
+// WiFi Config
+char ssid[] = "";  // WiFi SSID
+char pass[] = "";  // WiFI Password
