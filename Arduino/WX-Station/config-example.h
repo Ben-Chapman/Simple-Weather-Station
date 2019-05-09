e80b563b7232a821674f7274a5731715f0319282
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2019-05-09 13:04:48 -0400

    Adding support for debug-level logging

--- Arduino/WX-Station/config-example.h
+++ Arduino/WX-Station/config-example.h
@@ -10,3 +10,12 @@
 // WiFi Config
 char ssid[] = "";  // WiFi SSID
 char pass[] = "";  // WiFI Password
+
+// Providing for debug-level logging
+#ifdef DEBUG
+    #define debugPrint(x)    Serial.print(x)
+    #define debugPrintln(x)  Serial.println(x)
+#else
+    #define debugPrint(x)
+    #define debugPrintln(x)
+#endif
