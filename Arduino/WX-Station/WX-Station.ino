3ceb88dd6623a270fc0232c9fc0a1d7671109e26
Ben Chapman <5124569+Ben-Chapman@users.noreply.github.com>
2020-02-01 15:53:56 -0500

    Initial refactoring

--- Arduino/WX-Station/WX-Station.ino
+++ Arduino/WX-Station/WX-Station.ino
@@ -1,16 +1,19 @@
-#include <SPI.h>
 #include <Wire.h>
 #include "config.h"
 
 #include <ESP8266WiFi.h>
 
-// Env sensor Support
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
+#include "Adafruit_BMP3XX.h"
+#include "Adafruit_SHT31.h"
 
 // Adafruit.io Support
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+Adafruit_BMP3XX bmp; // I2C
 
 /*
   Global configs
@@ -18,12 +21,11 @@
 
 // Creating the array to be used for environmental sensor readings
 
-float measurementData[4] = {};
+float measurementData[3] = {};
 
 #define TEMPERATURE measurementData[0]
 #define HUMIDITY measurementData[1]
 #define PRESSURE measurementData[2]
-#define SOIL_MOISTURE measurementData[3]
 
 WiFiClient client;
 
@@ -32,103 +34,115 @@ Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO
 
 // Setup feeds for publishing
 Adafruit_MQTT_Publish temperaturec_feed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/weather-station.temperaturec");
-
 Adafruit_MQTT_Publish temperaturef_feed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/weather-station.temperaturef");
-
 Adafruit_MQTT_Publish humidity_feed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/weather-station.humidity");
-
 Adafruit_MQTT_Publish pressure_feed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/weather-station.pressure");
-
-Adafruit_MQTT_Publish soilmoisture_feed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/weather-station.soilmoisture");
-
 Adafruit_MQTT_Publish runtime_feed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/weather-station.debugruntime");
-
 Adafruit_MQTT_Publish sigstrength_feed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/weather-station.debugsigstrength");
 
 void MQTT_connect();
 
 void setup() {
-  #ifdef DEBUG
-    Serial.begin(115200);
-    Serial.println("Serial setup is connected");
-  #endif
+    #ifdef DEBUG
+        Serial.begin(115200);
+        Serial.println("Serial setup is connected");
+    #endif
 
-  // Connect to WiFi
-  unsigned long startTime = millis();
-  IPAddress ip(10, 0, 0, 201);
-  IPAddress gateway(10, 0, 0, 1);
-  IPAddress subnet(255, 255, 255, 0);
-  IPAddress DNS(10, 0, 0, 100);
-  int32_t chan = 11;  // Pre-defined (static) WiFi channel
+    // Connect to WiFi
+    unsigned long startTime = millis();
+    IPAddress ip(10, 0, 0, 202);
+    IPAddress gateway(10, 0, 0, 1);
+    IPAddress subnet(255, 255, 255, 0);
+    IPAddress DNS(10, 0, 0, 100);
+    int32_t chan = 11;  // Pre-defined (static) WiFi channel
 
-  WiFi.config(ip, gateway, subnet, DNS);
-  delay(100);
-  WiFi.begin(ssid, pass, chan);
+    WiFi.config(ip, gateway, subnet, DNS);
+    delay(50);
+    WiFi.begin(ssid, pass, chan);
 
-  // int status = WL_IDLE_STATUS;
-  debugPrint("Connecting to ");
-  debugPrintln(ssid);
-  while (WiFi.status() != WL_CONNECTED) {
-    debugPrint(".");
-    delay(500);
- }
+    // int status = WL_IDLE_STATUS;
+    debugPrint("Connecting to ");
+    debugPrintln(ssid);
+    while (WiFi.status() != WL_CONNECTED) {
+        debugPrint(".");
+        delay(500);
+    }
 
-  unsigned long finishTime = millis();
+    unsigned long finishTime = millis();
 
-  debugPrint("You're connected to the network: ");
-  debugPrintln(WiFi.localIP());
-  debugPrint("WiFi connection took ");
-  unsigned long timeTaken = ((finishTime - startTime) / 1000);
-  debugPrint(timeTaken);
-  debugPrintln(" seconds");
+    debugPrint("You're connected to the network: ");
+    debugPrintln(WiFi.localIP());
+    debugPrint("WiFi connection took ");
+    unsigned long timeTaken = ((finishTime - startTime) / 1000);
+    debugPrint(timeTaken);
+    debugPrintln(" seconds");
 }
 
 void loop() {
-  // Read measurements from enviromental sensor
-  readEnvironmentSensors();
+    // Read measurements from enviromental sensor
+    readEnvironmentSensors();
 
-  // Adafruit.io Publishing
-  MQTT_connect();
+    // Adafruit.io Publishing
+    // MQTT_connect();
 
-  temperaturec_feed.publish(int(TEMPERATURE));  // Degrees C
-  temperaturef_feed.publish(int((TEMPERATURE * 1.8)+ 32));  // Degrees F
-  humidity_feed.publish(int(HUMIDITY));
-  pressure_feed.publish(PRESSURE / 100);  // Millibar
-  soilmoisture_feed.publish(int(SOIL_MOISTURE));
-  runtime_feed.publish(int(millis()));
-  sigstrength_feed.publish(WiFi.RSSI());
+    // temperaturec_feed.publish(int(TEMPERATURE));  // Degrees C
+    // temperaturef_feed.publish(int((TEMPERATURE * 1.8)+ 32));  // Degrees F
+    // humidity_feed.publish(int(HUMIDITY));
+    // pressure_feed.publish(PRESSURE / 100);  // Millibar
+    // soilmoisture_feed.publish(int(SOIL_MOISTURE));
+    // runtime_feed.publish(int(millis()));
+    // sigstrength_feed.publish(WiFi.RSSI());
 
-  delay(1000);
-  debugPrintln("Entering deep sleep...");
+    delay(1000);
+    // debugPrintln("Entering deep sleep...");
 
-  // Sleeping
-  deepSleep(300);  
+    // Sleeping
+    // deepSleep(300);  
+
+
+
+
+
+    // Serial.print("Temperature from BMP = ");
+    // Serial.print(bmp.readTemperature());
+    // Serial.println(" *C");
+
+    // Serial.print("Temp *C from SenseIron = ");
+    // Serial.println(t);
+
+    // Serial.print("Hum. % from SenseIron = ");
+    // Serial.println(h);
+
+    // Serial.print("Pressure from BMP = ");
+    // Serial.print(bmp.readPressure() / 100.0);
+    // Serial.println(" hPa");
+
+    // Serial.println();
+    // delay(2000);
 }
 
 float readEnvironmentSensors() {
-  // Initializing the environment sensor
-  Adafruit_BME280 bme;  // Using I2C Connections
-  bme.begin(&Wire);
+    // Initializing the sensors
+    sht31.begin();
+    bmp.begin();
 
-  TEMPERATURE = bme.readTemperature();
-  HUMIDITY = bme.readHumidity();
-  PRESSURE = bme.readPressure();
-  /*
-  The soil moisture sensor returns high values in dry soil (<=786), and progressively lower values for wet soil (>=534). Inversing these values to make them a little more human platable. Wet == bigger number, dry smaller.
-  */
-  SOIL_MOISTURE = 1000 - analogRead(A0);  // Soil Moisture
+    // Set up oversampling and filter initialization
+    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+    bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
 
-  debugPrint("Temperature C: ");
-  debugPrintln(TEMPERATURE);
+    TEMPERATURE = sht31.readTemperature();
+    HUMIDITY = sht31.readHumidity();
+    PRESSURE = bmp.readPressure();
 
-  debugPrint("Humidity: ");
-  debugPrintln(HUMIDITY);
+    debugPrint("Temperature C: ");
+    debugPrintln(TEMPERATURE);
 
-  debugPrint("Pressure in Millibar: ");
-  debugPrintln(PRESSURE / 100);
+    debugPrint("Humidity: ");
+    debugPrintln(HUMIDITY);
 
-  debugPrint("Soil Moisture Level: ");
-  debugPrintln(SOIL_MOISTURE);
+    debugPrint("Pressure in Millibar: ");
+    debugPrintln(PRESSURE / 100);
 }
 
 void deepSleep(int sleepTimeInSec) {
